b8109937003a2770f434a08705be2e5d90297d4a
Falcon Dai <me@falcondai.com>
2017-10-07 21:34:15 +0800

    fix python 3 compat

--- util.py
+++ util.py
@@ -13,10 +13,9 @@ def system_exec(base_command, _out_path=None, **arg_value_dict):
     cmd = ' '.join([base_command, ' '.join(sampled_args)])
     if _out_path is not None:
         with open(_out_path, 'ab') as f:
-            f.write(cmd)
-            f.write('\n')
-    else:
-        print(cmd)
+            f.write(cmd.encode('utf8'))
+            f.write(b'\n')
+    print(cmd)
     return os.system(cmd)
 
 if __name__ == '__main__':
